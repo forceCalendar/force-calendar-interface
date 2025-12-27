5a50cecdfdb6da49701f33906bf88741bc826321
thedhanawada <wep.proto@gmail.com>
2025-12-27 20:02:29 +1100

    Add full calendar component using core

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // Force Calendar UI Components
 export { MonthView } from './components/MonthView.js';
+export { CalendarFull } from './components/CalendarFull.js';
 
 // Auto-register web components
 import './components/MonthView.js';
+import './components/CalendarFull.js';
