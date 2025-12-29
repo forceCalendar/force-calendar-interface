d49decff84903da016efca5c41abc1c1766b1b8d
thedhanawada <wep.proto@gmail.com>
2025-12-29 17:45:52 +1100

    Add calendar UI components using npm package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-// Force Calendar UI Components
+// forceCalendar UI - Enterprise Calendar Components
+// Powered by @forcecalendar/core npm package
+
+// Export all components
+export { CalendarView } from './components/CalendarView.js';
 export { MonthView } from './components/MonthView.js';
 export { CalendarFull } from './components/CalendarFull.js';
 
 // Auto-register web components
+import './components/CalendarView.js';
 import './components/MonthView.js';
 import './components/CalendarFull.js';
+
+console.log('forceCalendar UI v0.1.0 loaded');
